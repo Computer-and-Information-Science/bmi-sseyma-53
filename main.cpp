73873634be1896a7079acb72bffb3db0d54f5075
sseyma-53 <sseyma5317@gmail.com>
2025-09-09 18:47:20 +0000

    added code to compute bmi

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main() {
  
+    // declaring variables
 
+    int inches;
+    inches = 61;
+
+    int weight;
+    weight = 130;
+
+    double kgweight = weight*0.453592;
+
+    double heightM = inches*0.0254;
+
+    double BMI = kgweight/(heightM*heightM);
+
+    cout << "The BMI for this person is : " << 
+    BMI << "." << endl;
 
 }
